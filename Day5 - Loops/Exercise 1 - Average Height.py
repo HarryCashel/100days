3bb704a198a363819c19a56e56aa8fe64d2ab891
cashmoney <cashel.harry@gmail.com>
2021-09-22 21:33:01 +1000

    Day 5 - Exercise 1 - Average Height.py

---
+++ Day5 - Loops/Exercise 1 - Average Height.py
@@ -0,0 +1,28 @@
+"""Program to calculate the average height from a list of heights"""
+
+# Greeting
+
+print("Welcome to the average height calculator")
+
+add_height = True
+total_height = []
+while add_height:
+    height = input("Enter a height or press Enter to continue\n")
+    if height == "":
+        add_height = False
+    else:
+        try:
+
+            height = float(height)
+            total_height.append(height)
+
+        except ValueError:
+            print("That is not a number!")
+
+
+total_sum = 0
+for i in total_height:
+    total_sum += i
+
+average_height = total_sum/len(total_height)
+print(f"The average height is {average_height}m")
