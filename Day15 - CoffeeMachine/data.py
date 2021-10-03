2e4130beb1cedba4b50d1e1c08e60465e672b629
cashmoney <cashel.harry@gmail.com>
2021-10-03 18:32:35 +1100

    Day15 - Project. init commit + data

---
+++ Day15 - CoffeeMachine/data.py
@@ -0,0 +1,31 @@
+
+machine_resources = {
+    'water': 1000,
+    'milk': 1000,
+    'coffee': 240,
+}
+
+resource_costs = {
+    "espresso": {
+        'water': 0,
+        'milk': 50,
+        'coffee': 20,
+    },
+    "latte": {
+        'water': 200,
+        'milk': 150,
+        'coffee': 20,
+    },
+    "cappuccino": {
+        'water': 250,
+        'milk': 100,
+        'coffee': 20,
+    }
+}
+
+money_cost = {
+    "espresso": 1.50,
+    "latte": 2.50,
+    "cappuccino": 3.50,
+
+}
