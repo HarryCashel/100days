cc4c79d4e37f157e4261ca9b28fa91f3e528f3f3
cashmoney <cashel.harry@gmail.com>
2021-10-05 06:51:06 +1100

    Day - 18 - Turtle graphics

--- Day18 - OOP & GUI/Exercise3.py
+++ Day18 - OOP & GUI/Exercise3.py
@@ -3,7 +3,14 @@ from turtle import *
 tim = Turtle()
 tim.shape("turtle")
 
-angle = int(input("How many sides?"))
-for i in range(angle):
-    tim.fd(100)
-    tim.left(360/angle)
+
+def draw_shape(sides):
+    """Draws a shape with right angles with as many sides as input"""
+    angle = sides
+    for i in range(angle):
+        tim.fd(100)
+        tim.left(360/angle)
+
+
+for shapes in range(3,101):
+    draw_shape(shapes)
