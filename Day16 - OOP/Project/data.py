89456db040278fd162666ffe1c0dd39ac09e30e7
cashmoney <cashel.harry@gmail.com>
2021-10-04 14:36:30 +1100

    Coffee Machine in OOP

---
+++ Day16 - OOP/Project/data.py
@@ -0,0 +1,33 @@
+
+machine_resources = {
+    'water': 1000,
+    'milk': 1000,
+    'coffee': 250,
+}
+
+resource_costs = {
+    "espresso": {
+        'water': 0,
+        'milk': 50,
+        'coffee': 20,
+    },
+    "latte": {
+        'water': 200,
+        'milk': 150,
+        'coffee': 20,
+    },
+    "cappuccino": {
+        'water': 250,
+        'milk': 100,
+        'coffee': 20,
+    }
+}
+
+money_cost = {
+    "espresso": 1.50,
+    "latte": 2.50,
+    "cappuccino": 3.50,
+
+}
+
+bank = 0
