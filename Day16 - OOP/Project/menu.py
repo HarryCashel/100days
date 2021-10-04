805bde65f241366a66911e526d8c73d31af27b63
cashmoney <cashel.harry@gmail.com>
2021-10-04 15:51:37 +1100

    OOP menu

---
+++ Day16 - OOP/Project/menu.py
@@ -0,0 +1,41 @@
+class MenuItem:
+    """Models individual menu items"""
+    def __init__(self, name, water, milk, coffee, cost):
+        self.name = name
+        self.cost = cost
+        self.ingredients = {
+            "water": water,
+            "milk": milk,
+            "coffee": coffee,
+        }
+
+
+class Menu:
+    """Models the Menu with drink items"""
+    def __init__(self):
+        self.menu = [
+            MenuItem(name='latte', water=200, milk=150, coffee=20, cost=2.5),
+            MenuItem(name='cappuccino', water=250, milk=100, coffee=20, cost=3.5),
+            MenuItem(name='espresso', water=100, milk=0, coffee=20, cost=2),
+        ]
+
+    def get_items(self):
+        """Searches the menu and returns the names of available drinks"""
+        options = ""
+        for item in self.menu:
+            options += f"{item.name}/"
+        return options
+
+    def find_drink(self, choice):
+        """Searches for exact menu item by name.
+        Returns that item if exists in menu, otherwise returns None"""
+        for item in self.menu:
+            if item.name == choice:
+                return item
+            print("Sorry that item is not available")
+
+
+menu = Menu()
+
+print(menu.get_items())
+print(menu.find_drink('latte'))
