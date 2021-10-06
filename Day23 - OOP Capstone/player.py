4620c0e4de45ecd7d62b7ad6bfd403a78508857c
cashmoney <cashel.harry@gmail.com>
2021-10-06 13:17:12 +1100

    Player class

---
+++ Day23 - OOP Capstone/player.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+
+SPEED = 20
+
+
+class Player(Turtle):
+    """Models our movable player character"""
+    def __init__(self, colour):
+        super().__init__()
+        self.penup()
+        self.color(colour)
+        self.refresh()
+
+    def refresh(self):
+        """Refresh player position"""
+        self.goto(x=0, y=-340)
+        self.setheading(90)
+
+    def move_up(self):
+        """Moves player north"""
+        self.forward(SPEED)
+
+    def move_left(self):
+        """Moves player west"""
+        self.left(SPEED)
+
+    def move_right(self):
+        """Moves player east"""
+        self.right(SPEED)
