2b3318b4419a44df9750d20ae7499ed65e19c0ae
cashmoney <cashel.harry@gmail.com>
2021-10-06 15:33:27 +1100

    final

--- Day23 - OOP Capstone/main.py
+++ Day23 - OOP Capstone/main.py
@@ -1,8 +1,16 @@
+from scoreboard import ScoreBoard
 from turtle import Screen
 from player import Player
 from car import Car
 import random
 import time
+
+
+def level_up():
+    if player.ycor() > 340:
+        scoreboard.level_up()
+        player.refresh()
+
 
 DIFFICULTY = {'easy': 18, 'medium': 9, 'hard': 6, 'insane': 3}
 
@@ -18,6 +26,7 @@ screen.title("RUN")
 player = Player(screen.textinput(title="colour", prompt="What colour would you like?"))
 difficulty = DIFFICULTY[screen.textinput(title="select", prompt="'easy', 'medium', 'hard', or 'insane'")]
 car = Car()
+scoreboard = ScoreBoard()
 
 # Game conditional and logic
 test = True
@@ -29,17 +38,20 @@ screen.onkeypress(player.move_left, "a")
 screen.onkeypress(player.move_right, "d")
 
 while test:
-    time.sleep(.1)
-    screen.update()
     car.limit_cars()
     car.remove_car()
-    while len(car.all_cars) < 100:
-        if player.ycor() > 340:
-            player.refresh()
-        time.sleep(.1)
-        screen.update()
-        roll = random.randint(1, difficulty)
-        if roll < 2:
-            car.spawn_car()
-        car.move()
     car.move()
+    time.sleep(.1)
+    screen.update()
+    level_up()
+    for _ in car.all_cars:
+        if _.distance(player) < 20:
+            test = False
+
+    roll = random.randint(1, difficulty)
+    if roll < 2:
+        car.spawn_car()
+    car.move()
+
+scoreboard.game_over()
+screen.exitonclick()
