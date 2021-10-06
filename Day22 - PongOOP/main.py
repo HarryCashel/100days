8b649d8671957ce3e9b516ef81bc1b4bd7b81764
cashmoney <cashel.harry@gmail.com>
2021-10-06 12:56:30 +1100

    gameloop and conditions for scoring/end game

--- Day22 - PongOOP/main.py
+++ Day22 - PongOOP/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Player1, Player2
 from ball import Ball
+from scoreboard import ScoreBoard
 import time
 
 screen = Screen()
@@ -9,12 +10,12 @@ screen.title("Pong")
 screen.bgcolor("black")
 screen.tracer(0)
 
-
 p1 = Player1()
 p2 = Player2()
 ball = Ball()
-ball.speed("fastest")
+score = ScoreBoard()
 
+ball.speed("slow")
 
 screen.listen()
 screen.onkeypress(p1.move_up, "w")
@@ -25,18 +26,38 @@ screen.onkeypress(p2.move_down, "Down")
 game_on = True
 
 while game_on:
+    score.update_scoreboard()
     screen.update()
-    time.sleep(.1)
+
+    if score.left_score or score.right_score > 1:
+        game_on = False
+        ball.hideturtle()
+        score.game_over()
+        screen.update()
+    time.sleep(.08)
     ball.move_forward()
 
     # Check for wall collision
-    if abs(ball.ycor()) > 350:
+    if abs(ball.ycor()) > 335:
         new_heading = 360 - ball.heading()
         ball.setheading(new_heading)
 
     # Check for score
-    if abs(ball.xcor()) > 390:
+    if (ball.xcor()) > 390:
         ball.refresh()
+        score.increase_left_score()
+    elif ball.xcor() < -390:
+        ball.refresh()
+        score.increase_right_score()
 
+    # Check for paddle collision p1
+    if abs(ball.distance(p1)) < 30:
+        new_heading = 180 - ball.heading()
+        ball.setheading(new_heading)
+
+    # Check for paddle collision p2
+    if abs(ball.distance(p2)) < 30:
+        new_heading = 180 - ball.heading()
+        ball.setheading(new_heading)
 
 screen.exitonclick()
--- Day24 - Files, Directories and Paths/main.py
+++ Day24 - Files, Directories and Paths/main.py
@@ -15,7 +15,7 @@ food = Food()
 scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(snake.up, "Up")
+screen.onkeypress(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
