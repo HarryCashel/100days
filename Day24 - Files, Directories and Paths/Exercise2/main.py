06d6acdb7c610eb0adf9c94b945021932d8d00ad
cashmoney <cashel.harry@gmail.com>
2021-10-07 14:33:24 +1100

    before running main.py

--- Day24 - Files, Directories and Paths/Exercise2/main.py
+++ Day24 - Files, Directories and Paths/Exercise2/main.py
@@ -1,8 +1,16 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+
+# Use list comprehension to create a list of names from the text file
+los = [name.strip() for name in open("./Input/Names/invited_names.txt")]
+
+# Store the starting letter str in a variable
+with open("./Input/Letters/starting_letter.txt") as letter:
+    content = letter.read()
+
+# loop through each name in list and replace placeholder with personal name
+# write new file in /Output with name as file name
+for name in los:
+    file_with_name = content.replace("[name]", name)
+    print(file_with_name)
+    with open(f"./Output/ReadyToSend/LetterTo{name}", mode="w") as file:
+        file.write(file_with_name)
