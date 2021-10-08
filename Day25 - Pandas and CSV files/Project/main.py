5a736587592fbefc9c58ee14b5ea2697c7828a4c
cashmoney <cashel.harry@gmail.com>
2021-10-08 16:27:07 +1100

    Project start files, init commit

---
+++ Day25 - Pandas and CSV files/Project/main.py
@@ -0,0 +1,18 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("USA States")
+# screen.addshape("blank_states_img.gif")
+# turtle.shape("blank_states_img.gif")
+
+
+screen.bgpic(picname="blank_states_img.gif")
+
+# function to find x, y cords on the turtle screen
+# def mouse_coord(x, y):
+#     print(x, y)
+#
+# turtle.onscreenclick(mouse_coord)
+# turtle.mainloop()
+
+# screen.exitonclick()
