7642c5e54e5a1ed8b1aace16bca9cf22738b4ce3
cashmoney <cashel.harry@gmail.com>
2021-10-11 11:12:34 +1100

    button and input

--- Day27 - Tkinter Args and Kwargs/Exercise2.py
+++ Day27 - Tkinter Args and Kwargs/Exercise2.py
@@ -2,10 +2,10 @@
 
 """Tkinter gui exercises"""
 
-import tkinter
+from tkinter import *
 
 # Creates window object
-window = tkinter.Tk()
+window = Tk()
 
 # Give title
 window.title("Hello")
@@ -14,12 +14,30 @@ window.title("Hello")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="I am cool", font=("Ariel", 24, "italic"))
+my_label = Label(text="I am cool", font=("Ariel", 24, "italic"))
+my_label["text"] = "New Text"
+my_label.config(text="new_text")
 
 # Places a label and centers it
 my_label.pack(side="bottom", expand="True")
 
 
+# button function
+def button_clicked():
+    input_text = u_input.get()
+    my_label.config(text=input_text)
+    print(my_label["text"])
+
+
+# what happens when button is clicked
+button = Button(text="Click", command=button_clicked)
+button.pack()
+
+# Entry
+u_input = Entry(width=10)
+u_input.pack()
+
+print(u_input.get())
 
 # Always at the end of the tkinter program
 window.mainloop()
