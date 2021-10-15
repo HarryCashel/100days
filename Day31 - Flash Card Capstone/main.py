c8902e0446a06c5baed8a86d21523c0e83c27e11
cashmoney <cashel.harry@gmail.com>
2021-10-15 14:38:35 +1100

    init commit - Language Flash Card App

---
+++ Day31 - Flash Card Capstone/main.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+
+
+class MainUi:
+    def __init__(self, master):
+        pass
+
+    def button_click(self):
+        pass
+
+
+class PracticeWindow:
+    def __init__(self, master):
+        pass
+
+
+class CorrectWindow:
+    def __init__(self, master):
+        pass
+
+
+class WrongWindow:
+    def __init__(self, master):
+        pass
