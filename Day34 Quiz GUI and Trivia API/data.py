d3db9e152bdfbbe4ae6e80626f38e9bc9df02c03
cashmoney <cashel.harry@gmail.com>
2021-10-18 20:24:32 +1100

    Api request code

---
+++ Day34 Quiz GUI and Trivia API/data.py
@@ -0,0 +1,14 @@
+import requests
+
+URL = "https://opentdb.com/api.php?"
+
+parameters = {
+    "amount": "10",
+    "type": "boolean"
+}
+
+response = requests.get(url=URL, params=parameters)
+response.raise_for_status()
+
+data = response.json()["results"]
+print(data)
