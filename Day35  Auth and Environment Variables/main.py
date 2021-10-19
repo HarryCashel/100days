2f2858938e2df11567e4b3e437e8745374fd9cd1
cashmoney <cashel.harry@gmail.com>
2021-10-19 22:07:37 +1100

    check if raining in next 12 hours

--- Day35  Auth and Environment Variables/main.py
+++ Day35  Auth and Environment Variables/main.py
@@ -18,9 +18,27 @@ response.raise_for_status()
 data = response.json()
 current_temp = data["current"]["temp"]
 
-next_twelve_hour = data["hourly"][:13]
-hourly_weather_cond = [i["weather"][0]["description"] for i in next_twelve_hour]
-print(hourly_weather_cond)
+
+next_twelve_hour = data["hourly"][:12]
+
+# 0-th index is current hour
+hourly_weather_conds = [i["weather"][0]["description"] for i in next_twelve_hour]
+hourly_weather_codes = [i["weather"][0]["id"] for i in next_twelve_hour]
+
+print(hourly_weather_conds)
+print(hourly_weather_codes)
+
+
+will_rain = False
+
+for code in hourly_weather_codes:
+    if code < 700 and hourly_weather_codes.index(code) == 0:
+        print(f"It is raining")
+        will_rain = True
+    elif code < 700:
+        print(f"It is raining in {hourly_weather_codes.index(code)} hours")
+        will_rain = True
+
 
 
 
