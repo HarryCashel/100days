638ae4de75f4eaeae0e0a0c5ce52a6025a670c37
cashmoney <cashel.harry@gmail.com>
2021-10-20 13:42:14 +1100

    env variables + txt using twilio

--- Day35  Auth and Environment Variables/main.py
+++ Day35  Auth and Environment Variables/main.py
@@ -1,11 +1,17 @@
 import requests
+import os
+from twilio.rest import Client
 
 
-API_KEY = "b67aa4f693e4c12d819030c55159ee24"
+API_KEY = os.environ["WEATHER_API_KEY"]
 URL = "http://api.openweathermap.org/data/2.5/onecall"
-MY_LAT = -33.831830
-MY_LNG = 151.214243
+MY_LAT = -33.762232
+MY_LNG = 151.123619
 
+TWILIO_SID = os.environ["TWILIO_SID"]
+TWILIO_TOKEN = os.environ["TWILIO_TOKEN"]
+TWILIO_PH_NUM = "+18575778643"
+MY_NUM = os.environ["MY_NUM"]
 
 parameters = {
     "lat": MY_LAT,
@@ -30,14 +36,26 @@ print(hourly_weather_codes)
 
 
 will_rain = False
-
+message = ""
 for code in hourly_weather_codes:
     if code < 700 and hourly_weather_codes.index(code) == 0:
-        print(f"It is raining")
         will_rain = True
+        message = "It is raining"
     elif code < 700:
-        print(f"It is raining in {hourly_weather_codes.index(code)} hours")
+        message = f"It is raining in {hourly_weather_codes.index(code)} hours"
         will_rain = True
+
+if will_rain:
+
+    client = Client(TWILIO_SID, TWILIO_TOKEN)
+    message = client.messages \
+                    .create(
+                         body=message,
+                         from_=TWILIO_PH_NUM,
+                         to=MY_NUM
+                     )
+
+    print(message.status)
 
 
 
