5f89a5c4ff5f8de6838cd783e95e53a7aadbf5b8
cashmoney <cashel.harry@gmail.com>
2021-10-21 21:52:32 +1100

    Further changes noted

--- Day36 Stock Trader Alert/main.py
+++ Day36 Stock Trader Alert/main.py
@@ -66,8 +66,8 @@ def get_articles(info: dict):
 def get_recent_articles(info: dict) -> dict:
     data = {}
     for ticker in info:
-        most_recent_title = [i["title"] for i in info[ticker]["articles"][:3]]
-        most_recent_description = [i["description"] for i in info[ticker]["articles"][:3]]
+        most_recent_title = [i["title"] for i in info[ticker]["articles"][:2]]
+        most_recent_description = [i["description"] for i in info[ticker]["articles"][:2]]
         data[ticker] = {most_recent_title[i]: most_recent_description[i] for i in range(len(most_recent_title))}
     return data
 
@@ -110,26 +110,3 @@ def send_message(data: list):
 
 
 send_message(format_news())
-
-# for i in range(len(format_news())):
-#     print(format_news()[i])
-# STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-
-# STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-
-# STEP 3: Use https://www.twilio.com
-# Send a separate message with the percentage change and each article's title and description to your phone number.
-
-
-# Optional: Format the SMS message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
