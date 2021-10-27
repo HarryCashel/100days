a7b702f6ea93fa4e2bd8c5a67f244942745c65f5
cashmoney <cashel.harry@gmail.com>
2021-10-27 15:54:54 +1100

    test function to post query to trackapi

--- Day38 Exercise Tracker/main.py
+++ Day38 Exercise Tracker/main.py
@@ -4,8 +4,32 @@ import os
 
 today = datetime.today().strftime('%Y%m%d')
 
-KEY = os.environ["API_KEY"]
+TRACK_URL = "https://trackapi.nutritionix.com/v2/natural/exercise"
+API_KEY = os.environ["API_KEY"]
 APP_ID = os.environ["APP_ID"]
-USER_ID = 0
 
-# def test_endpoint():
+GOOGLE_API = os.environ["GOOGLE_API"]
+
+
+def test_endpoint():
+    exercise = input("What exercise? ")
+
+    headers = {
+        "x-app-id": APP_ID,
+        "x-app-key": API_KEY,
+    }
+    params = {
+        "query": exercise,
+        "gender": "male",
+        "weight_kg": 65,
+        "height_cm": 169,
+        "age": 27,
+    }
+
+    response = requests.post(url=TRACK_URL, headers=headers, json=params)
+    response.raise_for_status()
+
+    return response
+
+
+print(test_endpoint().text)
