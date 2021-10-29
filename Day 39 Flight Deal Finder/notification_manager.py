a926d383927c0e9801f617d34af68611cfecb0a5
cashmoney <cashel.harry@gmail.com>
2021-10-29 12:15:12 +1100

    Class to send messages via Twilio API

--- Day 39 Flight Deal Finder/notification_manager.py
+++ Day 39 Flight Deal Finder/notification_manager.py
@@ -1,3 +1,24 @@
+import os
+from twilio.rest import Client
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self):
+        self.message_list = []
+        self.sid = None
+        self.token = None
+        self.number = None
+        self.to_number = None
+
+    def send_message(self):
+        for flight in self.message_list:
+            message = flight
+            client = Client(self.sid, self.token)
+
+            message = client.messages \
+                .create(
+                    body=message,
+                    from_=self.number,
+                    to=self.to_number
+                )
+        print(message.status)
