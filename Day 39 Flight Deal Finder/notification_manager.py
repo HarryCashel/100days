75a351cd3de36b9bb68d2f4ea6d3d247ab5ea49d
cashmoney <cashel.harry@gmail.com>
2021-10-29 12:47:45 +1100

    added parameters to init

--- Day 39 Flight Deal Finder/notification_manager.py
+++ Day 39 Flight Deal Finder/notification_manager.py
@@ -3,12 +3,12 @@ from twilio.rest import Client
 
 
 class NotificationManager:
-    def __init__(self):
-        self.message_list = []
-        self.sid = None
-        self.token = None
-        self.number = None
-        self.to_number = None
+    def __init__(self, message_list, sid, token, number, to_number):
+        self.message_list = message_list
+        self.sid = sid
+        self.token = token
+        self.number = number
+        self.to_number = to_number
 
     def send_message(self):
         for flight in self.message_list:
