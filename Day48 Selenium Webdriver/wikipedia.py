a4f2161b7d98a51b62a3f12894f42eed54ef54cb
cashmoney <cashel.harry@gmail.com>
2021-11-14 13:20:42 +1100

    search and submit data using selenium

--- Day48 Selenium Webdriver/wikipedia.py
+++ Day48 Selenium Webdriver/wikipedia.py
@@ -5,11 +5,18 @@ from selenium.webdriver.common.by import By
 service = Service("C:/Users/cashe/Desktop/chromedriver")
 driver = webdriver.Chrome(service=service)
 
+# Get the url
 driver.get("https://www.wikipedia.org/")
 
+# Create selenium objects of our desired elements
 english_articles = driver.find_element(By.ID, "js-link-box-en")
 num_of_articles = english_articles.find_element(By.TAG_NAME, "small bdi")
 
+# Click link
 english_articles.click()
 
+# Locate search bar
+search_bar = driver.find_element(By.ID, "searchInput")
+search_bar.send_keys("Python")
+search_bar.submit()
 # driver.close()
