86e3bb3cf4bb7e1884364e0ba6e578f68dc7ae16
cashmoney <cashel.harry@gmail.com>
2021-11-17 20:56:34 +1100

    class declaration + init

--- Day52 Instagram Follower Bot/main.py
+++ Day52 Instagram Follower Bot/main.py
@@ -17,7 +17,18 @@ CHROME_DRIVER_PATH = "C:/Users/cashe/Desktop/chromedriver"
 INSTA_URL = "https://www.instagram.com/"
 
 
-# class InstagramBot:
-#     """Class to model an instagram user account and automatically follow accounts"""
-#     def __init__(self):
-#         self.service = Service(CHROME_DRIVER_PATH)
+class InstagramBot:
+    """Class to model an instagram user account and automatically follow accounts"""
+    def __init__(self):
+        self.service = Service(CHROME_DRIVER_PATH)
+        self.driver = webdriver.Chrome(service=self.service)
+        self.accounts_to_follow = "fraserwilson"
+
+    def sign_in(self):
+        """Method to open and sign into Instagram"""
+        self.driver.get("https://www.instagram.com/")
+        self.driver.fullscreen_window()
+
+
+# instabot = InstagramBot()
+# instabot.sign_in()
