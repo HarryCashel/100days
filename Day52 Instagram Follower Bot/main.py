1ac87a40e692046eeea6fac731d4adf631eca98a
cashmoney <cashel.harry@gmail.com>
2021-11-18 00:19:06 +1100

    completed method to search for single account

--- Day52 Instagram Follower Bot/main.py
+++ Day52 Instagram Follower Bot/main.py
@@ -8,7 +8,6 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 import os
 
-
 # Constants
 
 EMAIL = os.environ["EMAIL"]
@@ -19,6 +18,7 @@ INSTA_URL = "https://www.instagram.com/"
 
 class InstagramBot:
     """Class to model an instagram user account and automatically follow accounts"""
+
     def __init__(self):
         self.service = Service(CHROME_DRIVER_PATH)
         self.driver = webdriver.Chrome(service=self.service)
@@ -29,12 +29,27 @@ class InstagramBot:
         self.driver.get("https://www.instagram.com/")
         self.driver.fullscreen_window()
 
+        email_xpath = '//*[@id="loginForm"]/div/div[1]/div/label/input'
+        email_field = self.wait_for_field(email_xpath)
+        email_field.send_keys(EMAIL)
+
+        password_xpath = '//*[@id="loginForm"]/div/div[2]/div/label/input'
+        password_field = self.wait_for_field(password_xpath)
+        password_field.send_keys(PASSWORD)
+
+        password_field.send_keys(Keys.RETURN)
+
     def search(self):
         search_bar_xpath = '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input'
         search_bar = self.wait_for_field(xpath=search_bar_xpath)
         search_bar.send_keys(self.accounts_to_follow)
-        search_bar.send_keys(Keys.RETURN)
 
+        # somewhat verifies first link is accurate to what we are looking for
+        WebDriverWait(self.driver, 120).until(
+            EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, self.accounts_to_follow)))
+        first_result_link = self.wait_for_field(
+            '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[3]/div/div[2]/div/div[1]/a')
+        first_result_link.click()
 
     def get_to_followers(self):
         pass
@@ -47,6 +62,7 @@ class InstagramBot:
 
         field = self.driver.find_element(By.XPATH, xpath)
         return field
+
 
 instabot = InstagramBot()
 instabot.sign_in()
