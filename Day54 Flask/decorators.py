8f7936dd49904095c816c9eef5d4bb1adb356d52
cashmoney <cashel.harry@gmail.com>
2021-11-18 19:37:31 +1100

    basic flask app and practicing function decorators

---
+++ Day54 Flask/decorators.py
@@ -0,0 +1,29 @@
+import time
+
+current_time = time.time()
+print(current_time)
+
+
+def speed_calc_decorator(function):
+    def wrapper_function():
+        before_time = time.time()
+        function()
+        after_time = time.time()
+        print(f"{function.__name__} run speed: {after_time - before_time}")
+    return wrapper_function
+
+
+@speed_calc_decorator
+def fast_function():
+    for i in range(10000000):
+        i * i
+
+
+@speed_calc_decorator
+def slow_function():
+    for i in range(100000000):
+        i * i
+
+
+(slow_function())
+(fast_function())
