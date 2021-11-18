53fddf910f8f5f6254147a814cbf593195cbb14a
cashmoney <cashel.harry@gmail.com>
2021-11-18 22:12:40 +1100

    render html files with flask render static files with flask

---
+++ Day56 Website Templates + Flask/server.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home(name=None):
+    return render_template("home.html", name=name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
