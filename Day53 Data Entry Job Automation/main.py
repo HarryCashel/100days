5ed38e16a15bb60f036db5e050c524b92b8c598e
cashmoney <cashel.harry@gmail.com>
2021-11-18 15:10:36 +1100

    class and method to scrape for rental listings

--- Day53 Data Entry Job Automation/main.py
+++ Day53 Data Entry Job Automation/main.py
@@ -24,8 +24,33 @@ ZILLOW_URL = "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7
              "%22value%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D" \
              "%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min" \
              "%22%3A1%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A12%7D "
+CHROME_DRIVER_PATH = "C:/Users/cashe/Desktop/chromedriver"
 
 
+class DataEntry:
+    def __init__(self):
+        self.header = browser_headings = {
+            "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/95.0.4638.69 Safari/537.36 "
+        }
+        self.service = Service(CHROME_DRIVER_PATH)
+        self.driver = webdriver.Chrome(service=self.service)
 
+    # method for scraping
+    def get_listings(self):
+        response = requests.get(url=ZILLOW_URL, headers=self.header).text
+        soup = BeautifulSoup(response, "html.parser")
 
+        rental_links = soup.select(".list-card-info")
+        rental_links.remove(rental_links[-1])
 
+        house_data = [{
+            "price": link.findChild(class_="list-card-price").text.replace("/$+", ""),
+            "address": link.findChild(class_="list-card-addr").text,
+            "link": link.findChild(class_="list-card-link")["href"]
+        } for link in rental_links]
+
+        return house_data
+
+# print(rental_links)
