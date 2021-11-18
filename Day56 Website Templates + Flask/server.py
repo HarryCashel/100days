35f4eea37ecf43936671c9ac8bbeff61f18d421b
cashmoney <cashel.harry@gmail.com>
2021-11-18 22:23:17 +1100

    simple rendering with flask

--- Day56 Website Templates + Flask/server.py
+++ Day56 Website Templates + Flask/server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home(name=None):
-    return render_template("home.html", name=name)
+    return render_template("index.html", name=name)
 
 
 if __name__ == "__main__":
