7af5063a19cb0491769c8c8d042023c07f8e15b9
cashmoney <cashel.harry@gmail.com>
2021-11-19 15:49:10 +1100

    blog object

--- Day57 zBlog Capstone Part 1 - Templating/main.py
+++ Day57 zBlog Capstone Part 1 - Templating/main.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
-
+import requests
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_url = "https://api.npoint.io/60f451367a93defd85d2"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("index.html", posts=all_posts,)
+
+
+@app.route('/post/<int:blog_id>')
+def blog(blog_id):
+    blog_url = "https://api.npoint.io/60f451367a93defd85d2"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("index.html", posts=all_posts, id=blog_id)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
