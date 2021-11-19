a9efe82989a375bafb5ed5cea666a7436282a433
cashmoney <cashel.harry@gmail.com>
2021-11-19 15:49:45 +1100

    post class

--- Day57 zBlog Capstone Part 1 - Templating/post.py
+++ Day57 zBlog Capstone Part 1 - Templating/post.py
@@ -1,6 +1,6 @@
 class Post:
-    def __init__(self,  id, title, subtitle, body):
-        self.id = id
+    def __init__(self,  post_id, title, subtitle, body):
+        self.post_id = post_id
         self.title = title
         self.subtitle = subtitle
         self.body = body
