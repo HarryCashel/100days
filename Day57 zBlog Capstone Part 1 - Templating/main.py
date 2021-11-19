e3ae3ec59998b851b0d065783e12389c227826d2
cashmoney <cashel.harry@gmail.com>
2021-11-19 22:28:03 +1100

    rendering with jinja + creating routes with flask

--- Day57 zBlog Capstone Part 1 - Templating/main.py
+++ Day57 zBlog Capstone Part 1 - Templating/main.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template
+from post import Post
 import requests
 
 app = Flask(__name__)
 
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+
+post_objects = [Post(post["id"], post["title"], post["subtitle"], post["body"]) for post in posts]
+
 
 @app.route('/')
 def home():
-    blog_url = "https://api.npoint.io/60f451367a93defd85d2"
-    response = requests.get(blog_url)
-    all_posts = response.json()
-    return render_template("index.html", posts=all_posts,)
+    return render_template("index.html", posts=post_objects, )
 
 
 @app.route('/post/<int:blog_id>')
 def blog(blog_id):
-    blog_url = "https://api.npoint.io/60f451367a93defd85d2"
-    response = requests.get(blog_url)
-    all_posts = response.json()
-    return render_template("index.html", posts=all_posts, id=blog_id)
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.post_id == blog_id:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 
 if __name__ == "__main__":
