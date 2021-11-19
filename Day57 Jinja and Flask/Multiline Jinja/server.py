526554be9f0a7b48b1d10b852eaf49860454cfe2
cashmoney <cashel.harry@gmail.com>
2021-11-19 15:09:51 +1100

    multi-line statements w/ Jinja

---
+++ Day57 Jinja and Flask/Multiline Jinja/server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/blog")
+def blog():
+    blog_url = "https://api.npoint.io/60f451367a93defd85d2"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
