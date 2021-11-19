279bd179fccbb02162fca236c3b9e82ba64178da
cashmoney <cashel.harry@gmail.com>
2021-11-19 15:16:28 +1100

    edits

--- Day57 Jinja and Flask/Multiline Jinja/server.py
+++ Day57 Jinja and Flask/Multiline Jinja/server.py
@@ -9,12 +9,12 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/blog")
-def blog():
+@app.route("/blog/<idd>")
+def blog(idd):
     blog_url = "https://api.npoint.io/60f451367a93defd85d2"
     response = requests.get(blog_url)
     all_posts = response.json()
-    return render_template("blog.html", posts=all_posts)
+    return render_template("blog.html", posts=all_posts, id=idd)
 
 
 if __name__ == "__main__":
