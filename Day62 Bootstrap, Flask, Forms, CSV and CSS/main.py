9c971daaa9ab849c5885f983942fa484de1d0bb8
cashmoney <cashel.harry@gmail.com>
2021-11-23 13:32:47 +1100

    quick form using flask to add cafesto csv file

--- Day62 Bootstrap, Flask, Forms, CSV and CSS/main.py
+++ Day62 Bootstrap, Flask, Forms, CSV and CSS/main.py
@@ -38,14 +38,21 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/add')
+@app.route('/add', methods=["GET", "POST"])
 def add_cafe():
     form = CafeForm()
     if form.validate_on_submit():
-        print("True")
-    # Exercise:
-    # Make the form write a new row into cafe-data.csv
-    # with   if form.validate_on_submit()
+        with open('cafe-data.csv', mode="a", encoding="utf8") as csv_file:
+            csv_file.write(
+                f"\n{form.cafe.data},"
+                f"{form.location.data},"
+                f"{form.open.data},"
+                f"{form.close.data},"
+                f"{form.coffee_rating.data},"
+                f"{form.wifi_rating.data},"
+                f"{form.power.data}"
+            )
+            return redirect(url_for('cafes'))
     return render_template('add.html', form=form)
 
 
